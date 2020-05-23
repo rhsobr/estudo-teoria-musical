6cbba77a86f3d8151e33db017d535ffb84133629
rhsobr <rhsobr@gmail.com>
2020-05-23 14:47:35 -0300

    🎨 ✨ -

---
+++ escalas.ts
@@ -0,0 +1,132 @@
+import {
+  NOTA_SIMPLES,
+  NOTA_INTERVALO,
+  MODIFICADOR_NOTA_SIMPLES,
+  NOTA,
+  PEGA_TODAS_NOTAS,
+} from "./notas";
+
+class Escala {
+  definicao: Array<NOTA_INTERVALO>;
+  todas_notas: Array<NOTA>;
+
+  constructor(definicao: Array<NOTA_INTERVALO>) {
+    if (!definicao.length) {
+      throw new Error("Definição precisa ter intervalos");
+    }
+
+    this.definicao = definicao;
+    this.todas_notas = PEGA_TODAS_NOTAS();
+  }
+
+  private pegaNovaPosicao(posicao_atual: any, intervalo: NOTA_INTERVALO) {
+    let nova_posicao = posicao_atual;
+
+    //const aumenta = parseInt(intervalo, 10);
+
+    for (var i = 0; i < intervalo; i++) {
+      nova_posicao++;
+
+      if (this.todas_notas.length == nova_posicao) {
+        nova_posicao = 0;
+      }
+    }
+
+    return nova_posicao;
+  }
+
+  private defineEscala = (definicao: Array<NOTA_INTERVALO>) => (
+    nota: NOTA_SIMPLES,
+    modificador?: MODIFICADOR_NOTA_SIMPLES
+  ): NOTA[] => {
+    const posicao_tonica = this.todas_notas.findIndex(
+      (metadado) =>
+        metadado.nota === nota &&
+        (!modificador || metadado.modificadores.includes(modificador))
+    );
+
+    let posicao = posicao_tonica;
+
+    const tonica = this.todas_notas[posicao_tonica];
+
+    const todas_notas = definicao.reduce(
+      (acc, intervalo) => {
+        posicao = this.pegaNovaPosicao(posicao, intervalo);
+        acc.push(this.todas_notas[posicao]);
+        return acc;
+      },
+      [tonica]
+    );
+
+    //remove última nota
+    todas_notas.pop();
+
+    return todas_notas;
+  };
+
+  com_tonica(tonica: NOTA): NOTA[] {
+    return this.defineEscala(this.definicao)(
+      tonica.nota,
+      tonica.modificadores[0]
+    );
+  }
+}
+
+export default {
+  maior: {
+    natural: new Escala([
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.SEMITOM,
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.SEMITOM,
+    ]),
+  },
+  menor: {
+    natural: new Escala([
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.SEMITOM,
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.SEMITOM,
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.TOM,
+    ]),
+    harmonica: new Escala([
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.SEMITOM,
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.SEMITOM,
+      NOTA_INTERVALO.TOM + NOTA_INTERVALO.SEMITOM,
+      NOTA_INTERVALO.SEMITOM,
+    ]),
+    melodica: new Escala([
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.SEMITOM,
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.SEMITOM,
+    ]),
+  },
+  pentatonica: {
+    maior: new Escala([
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.TOM + NOTA_INTERVALO.SEMITOM,
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.TOM + NOTA_INTERVALO.SEMITOM,
+    ]),
+    menor: new Escala([
+      NOTA_INTERVALO.TOM + NOTA_INTERVALO.SEMITOM,
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.TOM,
+      NOTA_INTERVALO.TOM + NOTA_INTERVALO.SEMITOM,
+      NOTA_INTERVALO.TOM,
+    ]),
+  },
+};
--- index.ts
+++ index.ts
@@ -1,121 +1,60 @@
-import {
-  NOTA_SIMPLES,
-  NOTA,
-  MODIFICADOR_NOTA_SIMPLES,
-  NOTA_INTERVALO,
-} from "./notas";
+import escalas from "./escalas";
+import { PEGA_TODAS_NOTAS } from "./notas";
 
-const TODAS_NOTAS = [
-  new NOTA(NOTA_SIMPLES.C),
-  new NOTA(NOTA_SIMPLES.C, MODIFICADOR_NOTA_SIMPLES["#"]),
-  new NOTA(NOTA_SIMPLES.D),
-  new NOTA(NOTA_SIMPLES.D, MODIFICADOR_NOTA_SIMPLES["#"]),
-  new NOTA(NOTA_SIMPLES.E),
-  new NOTA(NOTA_SIMPLES.F),
-  new NOTA(NOTA_SIMPLES.F, MODIFICADOR_NOTA_SIMPLES["#"]),
-  new NOTA(NOTA_SIMPLES.G),
-  new NOTA(NOTA_SIMPLES.G, MODIFICADOR_NOTA_SIMPLES["#"]),
-  new NOTA(NOTA_SIMPLES.A),
-  new NOTA(NOTA_SIMPLES.A, MODIFICADOR_NOTA_SIMPLES["#"]),
-  new NOTA(NOTA_SIMPLES.B),
-];
-
-const pegaNovaPosicao = (posicao_atual: any, diferenca: NOTA_INTERVALO) => {
-  let nova_posicao = posicao_atual;
-  let aumenta = 1;
-
-  if (diferenca === NOTA_INTERVALO.TOM) {
-    aumenta = 2;
-  }
-
-  for (var i = 0; i < aumenta; i++) {
-    nova_posicao++;
-
-    if (TODAS_NOTAS.length == nova_posicao) {
-      nova_posicao = 0;
-    }
-  }
-
-  return nova_posicao;
-};
-
-const defineEscala = (definicao: Array<NOTA_INTERVALO>) => (
-  nota: NOTA_SIMPLES,
-  modificador?: MODIFICADOR_NOTA_SIMPLES
-) => {
-  const posicao_tonica = TODAS_NOTAS.findIndex(
-    (metadado) =>
-      metadado.nota === nota &&
-      (!modificador || metadado.modificadores.includes(modificador))
-  );
-
-  let posicao = posicao_tonica;
-
-  const tonica = TODAS_NOTAS[posicao_tonica];
-
-  const todas_notas = definicao.reduce(
-    (acc, item) => {
-      posicao = pegaNovaPosicao(posicao, item);
-      acc.push(TODAS_NOTAS[posicao]);
-      return acc;
-    },
-    [tonica]
-  );
-
-  //remove última nota
-  todas_notas.pop();
-
-  return todas_notas;
-};
-
-const escalaMaior = (
-  nota: NOTA_SIMPLES,
-  modificador?: MODIFICADOR_NOTA_SIMPLES
-) => {
-  const definicao = [
-    NOTA_INTERVALO.TOM,
-    NOTA_INTERVALO.TOM,
-    NOTA_INTERVALO.SEMITOM,
-    NOTA_INTERVALO.TOM,
-    NOTA_INTERVALO.TOM,
-    NOTA_INTERVALO.TOM,
-    NOTA_INTERVALO.SEMITOM,
-  ];
-
-  return defineEscala(definicao)(nota, modificador);
-};
-
-const escalaMenor = (
-  nota: NOTA_SIMPLES,
-  modificador?: MODIFICADOR_NOTA_SIMPLES
-) => {
-  const definicao = [
-    NOTA_INTERVALO.TOM,
-    NOTA_INTERVALO.SEMITOM,
-    NOTA_INTERVALO.TOM,
-    NOTA_INTERVALO.TOM,
-    NOTA_INTERVALO.SEMITOM,
-    NOTA_INTERVALO.TOM,
-    NOTA_INTERVALO.TOM,
-  ];
-
-  return defineEscala(definicao)(nota, modificador);
-};
+const TODAS_NOTAS = PEGA_TODAS_NOTAS();
 
 TODAS_NOTAS.forEach((nota_composta) => {
   console.log(
-    `ESCALA MAIOR ${nota_composta.nome}`,
-    escalaMaior(nota_composta.nota, nota_composta.modificadores[0]).map(
-      (item) => item?.nome
-    )
+    `ESCALA MAIOR NATURAL ${nota_composta.nome} =>`,
+    escalas.maior.natural.com_tonica(nota_composta).map((item) => item?.nome)
   );
 });
 
+console.debug("-------");
+
 TODAS_NOTAS.forEach((nota_composta) => {
   console.log(
-    `ESCALA MENOR NATURAL ${nota_composta.nome}`,
-    escalaMenor(nota_composta.nota, nota_composta.modificadores[0]).map(
-      (item) => item?.nome
-    )
+    `ESCALA MENOR NATURAL ${nota_composta.nome} =>`,
+    escalas.menor.natural.com_tonica(nota_composta).map((item) => item?.nome)
+  );
+});
+
+console.debug("-------");
+
+TODAS_NOTAS.forEach((nota_composta) => {
+  console.log(
+    `ESCALA MENOR HARMÔNICA ${nota_composta.nome} =>`,
+    escalas.menor.harmonica.com_tonica(nota_composta).map((item) => item?.nome)
+  );
+});
+
+console.debug("-------");
+
+TODAS_NOTAS.forEach((nota_composta) => {
+  console.log(
+    `ESCALA MENOR MELODICA ${nota_composta.nome} =>`,
+    escalas.menor.melodica.com_tonica(nota_composta).map((item) => item?.nome)
+  );
+});
+
+console.debug("-------");
+
+TODAS_NOTAS.forEach((nota_composta) => {
+  console.log(
+    `ESCALA PENTATÔNICA MAIOR ${nota_composta.nome} =>`,
+    escalas.pentatonica.maior
+      .com_tonica(nota_composta)
+      .map((item) => item?.nome)
+  );
+});
+
+console.debug("-------");
+
+TODAS_NOTAS.forEach((nota_composta) => {
+  console.log(
+    `ESCALA PENTATÔNICA MENOR ${nota_composta.nome} =>`,
+    escalas.pentatonica.menor
+      .com_tonica(nota_composta)
+      .map((item) => item?.nome)
   );
 });
